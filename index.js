36f0c69b4c7f3d2d042ace2c0c66a26a74c32f5f
Siddhant Shukla <140405534+SiddhantShukla1609@users.noreply.github.com>
2024-02-07 22:06:44 +0530

    Add files via upload
    css and js file added

---
+++ index.js
@@ -0,0 +1,12 @@
+function jk () {
+
+
+    document.querySelector(".question q-1").innerHTML=
+    <p class="answer"><span class="a-1">It's a super easy process to create a wedding invitation with Celebrare: </span> 
+                                        <span class="a-1-1">1. Select the template</span><br>
+                                        <span class="a-1-2">2. Click on "Try this card"</span></br>
+                                        <span class="a-1-3">3. Fill the form with your details</span><br>
+                                        <span class="a-1-4">4. Click on the "Share" button</span></br>
+                                        <span class="a-1-5">5. Make the payment and download the card</span>
+                                    </p>
+}
